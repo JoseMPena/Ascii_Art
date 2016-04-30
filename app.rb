3b8cc911791281f5ed42ee95d9ce51b505c512d5
LegaTux <josmpena91@gmail.com>
2016-04-30 21:50:46 +0200

    Add easter egg

---
+++ app.rb
@@ -0,0 +1,20 @@
+require "sinatra"
+require "sinatra/reloader" if development?
+require "artii"
+
+# get "/" do
+#   erb(:index)
+# end
+
+get "/ascii/?:p1?/?:p2?/?:p3?" do
+  if params[:p1] == "falcon" &&
+      params[:p2] == "special" &&
+      params[:p3] == "secret"
+    erb :easter_egg
+    else
+      artii = Artii::Base.new font: "epic"
+      @word = artii.asciify(params[:p1])
+      @font = artii.asciify(params[:p2])
+      erb :ascii
+  end
+end
